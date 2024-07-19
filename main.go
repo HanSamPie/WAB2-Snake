87fa30e592e8d005b489ce8a32190a331521506a
HanSamPie <hanpie0501@gmail.com>
2024-07-19 21:51:53 +0200

    removed init function from main

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"projects/game"
 	"projects/render"
 )
 
-func initGame(columns int, rows int, debug bool) *game.GameState {
-	return game.InitGame(columns, rows, debug)
-}
-
 func main() {
-	fmt.Println("hello there")
-
-	gameState := initGame(9, 9, true)
-
+	gameState := game.InitGame(15, 15, true)
 	render.Main(gameState)
 }
